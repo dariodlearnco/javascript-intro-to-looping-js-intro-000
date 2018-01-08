3e72636b00926fefa4171bfbad72dd2cdd17a41f
dariodlearnco <dario.dorella@gmail.com>
2018-01-08 03:10:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,26 +5,3 @@ function forLoop(array) {
     }
     return array
 }
-
-function whileLoop(n) {
-    var i = n
-    while ( i > 0 ) {
-        console.log(`${i}...`)
-        i--
-    }
-    return 'done'
-}
- 
-
-// function maybeTrue() {
-//   return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
-// }
-
-function doWhileLoop(array) {
-    // removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`
-    do {
-        array.pop()
-    }
-    while ( (array.lenght > 0) && (maybeTrue()) ) {
-    return array
-}
